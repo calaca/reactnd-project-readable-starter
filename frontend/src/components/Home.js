6991b6712358e6bc55ab42a00114926c93316a30
lorena <lorena.calaca.od@gmail.com>
2018-05-01 23:15:13 -0300

    feat: add Posts API util

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,5 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { getPosts } from '../util/PostsAPI';
+import sortBy from 'sort-by';
 
-const Home = () => <section className="home">Home</section>;
+class Home extends Component {
+  state = {
+    posts: []
+  }
+
+  componentDidMount() {
+    getPosts()
+      .then(posts => {
+        posts.sort(sortBy('title'));
+        this.setState({ posts });
+      }
+    )
+  }
+
+  render() {
+    return (
+      <section className="home">
+        Home
+      </section>
+    )
+  }
+}
 
 export default Home;
