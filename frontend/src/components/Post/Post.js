ea9cbe33c9e814fe969d5175980f9630daf0e31e
lorena <lorena.calaca.od@gmail.com>
2018-05-05 15:47:06 -0300

    styles: update component to stateless functional component

--- frontend/src/components/Post/Post.js
+++ frontend/src/components/Post/Post.js
@@ -1,38 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { submitPostVoteScore } from '../../actions/PostActions';
 import './Post.css';
 
-class Post extends Component {
-  render() {
-    const { post, dispatch } = this.props;
-
-    return (
-      <article className="post">
-        <div className="vote">
-          <span className="up" onClick={() => { dispatch(submitPostVoteScore(post.id, 'upVote')) }}></span>
-          <span className="count">{post.voteScore}</span>
-          <span className="down" onClick={() => { dispatch(submitPostVoteScore(post.id, 'downVote')) }}></span>
-        </div>
-        <div className="thumbnail"></div>
-        <div className="content">
-          <h3 className="title"><Link to={`/${post.category}/${post.id}`}>{post.title}</Link></h3>
-          <p className="details">submitted by <span>{post.author}</span></p>
-          <div className="comments">
-            <p>
-              <span className="count">{post.commentCount}</span> comments
+const Post = ({ post, dispatch }) => (
+  <article className="post">
+    <div className="vote">
+      <span className="up" onClick={() => { dispatch(submitPostVoteScore(post.id, 'upVote')) }}></span>
+      <span className="count">{post.voteScore}</span>
+      <span className="down" onClick={() => { dispatch(submitPostVoteScore(post.id, 'downVote')) }}></span>
+    </div>
+    <div className="thumbnail"></div>
+    <div className="content">
+      <h3 className="title"><Link to={`/${post.category}/${post.id}`}>{post.title}</Link></h3>
+      <p className="details">submitted by <span>{post.author}</span></p>
+      <div className="comments">
+        <p>
+          <span className="count">{post.commentCount}</span> comments
             </p>
-          </div>
-          <div className="info-actions">
-            <span className="category">{post.category}</span>
-            <button className="edit">edit</button>
-            <button className="delete">delete</button>
-          </div>
-        </div>
-      </article>
-    )
-  }
-}
+      </div>
+      <div className="info-actions">
+        <span className="category">{post.category}</span>
+        <button className="edit">edit</button>
+        <button className="delete">delete</button>
+      </div>
+    </div>
+  </article>
+);
 
 export default connect()(Post);
