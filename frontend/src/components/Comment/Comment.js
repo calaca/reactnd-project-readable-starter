19fa260c8d9e98477776d7a503adec2bd2e64029
lorena <lorena.calaca.od@gmail.com>
2018-05-05 21:46:52 -0300

    styles: update Comment component to stateless functional component

--- frontend/src/components/Comment/Comment.js
+++ frontend/src/components/Comment/Comment.js
@@ -1,45 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import dateFormatter from '../../helpers/dateFormatter';
 import { submitCommentVoteScore } from '../../actions/CommentActions';
 import './Comment.css';
 
-class Comment extends Component {
-
-  render() {
-    const { comment, dispatch } = this.props;
-
-    return (
-      <article className="comment">
-        <div className="vote">
-          <span
-            className="up"
-            onClick={() => { dispatch(submitCommentVoteScore(comment.id, 'upVote')) }}
-          >
-          </span>
-          <span className="count">{comment.voteScore}</span>
-          <span
-            className="down"
-            onClick={() => { dispatch(submitCommentVoteScore(comment.id, 'downVote')) }}
-          >
-          </span>
-        </div>
-        <div className="content">
-          <div className="author-details">
-            <h3 className="author-name">{comment.author}</h3>
-            <span className="date">{`${dateFormatter(comment.timestamp)} ago`}</span>
-          </div>
-          <p className="body">
-            {comment.body}
-          </p>
-          <div className="info-actions">
-            <button className="edit">edit</button>
-            <button className="delete">delete</button>
-          </div>
-        </div>
-      </article>
-    )
-  }
-}
+const Comment = ({ comment, dispatch }) => (
+  <article className="comment">
+    <div className="vote">
+      <span
+        className="up"
+        onClick={() => { dispatch(submitCommentVoteScore(comment.id, 'upVote')) }}
+      >
+      </span>
+      <span className="count">{comment.voteScore}</span>
+      <span
+        className="down"
+        onClick={() => { dispatch(submitCommentVoteScore(comment.id, 'downVote')) }}
+      >
+      </span>
+    </div>
+    <div className="content">
+      <div className="author-details">
+        <h3 className="author-name">{comment.author}</h3>
+        <span className="date">{`${dateFormatter(comment.timestamp)} ago`}</span>
+      </div>
+      <p className="body">
+        {comment.body}
+      </p>
+      <div className="info-actions">
+        <button className="edit">edit</button>
+        <button className="delete">delete</button>
+      </div>
+    </div>
+  </article>
+);
 
 export default connect()(Comment);
